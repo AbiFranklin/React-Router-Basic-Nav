41a942e18120f34c5482c8e64a41a46cc04cf1cc
Abigayle Franklin <abifranklin@gmail.com>
2018-10-18 15:42:52 -0500

    Styled with Material-UI

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { createMuiTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Menu from '@material-ui/core/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Button from '@material-ui/core/Button';
+import { purple, lightGreen } from '@material-ui/core/colors';
+import styled from 'styled-components';
+
+const NavLeft = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: start;
+  padding: 20px;
+`
+const NavRight = styled.div`
+  width: 40%;
+  display: flex;
+  justify-content: space-around;
+  padding: 20px;
+  `
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: lightGreen
+  }
+})
+
 
 const Navigation = () => {
   return (
     <div>
-      <div className="App">
-        <h1>React Router Mini</h1>
-        <div>
-          <Link to='/'>Home</Link>
-        </div>
-        <div>
-          <Link to='/about'>About</Link>
-        </div>
-        <div>
-          <Link to='/contact'>Contact</Link>
-        </div>
-      </div>
+      <AppBar position="static">
+        <Toolbar>
+          <NavLeft>
+        <Typography variant="h2" color="inherit" noWrap>
+            React Router Mini
+          </Typography>
+          </NavLeft>
+          <NavRight>
+          <Link to='/'><Button variant="raised" color="secondary">Home</Button></Link>
+          <Link to='/about'><Button variant="raised" color="secondary">About</Button></Link>
+          <Link to='/contact'><Button variant="raised" color="secondary">Contact</Button></Link>
+          </NavRight>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router } from "react-router-dom";
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import { purple, lightGreen } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: lightGreen
+  }
+})
 
 ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
 <Router>
     <App />
-</Router>
+</Router></MuiThemeProvider>
 , document.getElementById('root'));
